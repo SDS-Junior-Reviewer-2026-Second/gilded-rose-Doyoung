b8e8964e293616fe927d4d6fe6fa7c2d5fc0f9e1
kimdoyoung0319 <kimdoyoung0319@gmail.com>
2026-09-16 13:34:16 +0900

    refactored GildedRose.
    - Introduced a factory method for Item class
    - Replaced manual string comparision into subclass
    - Moved logics regarding to the update of quality and sellIn to each
      subclass' advanceDay method

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,6 +1,6 @@
 package com.gildedrose;
 
-public class Item {
+public abstract class Item {
 
     public String name;
 
@@ -14,8 +14,100 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public abstract void advanceDay();
+
+    public static Item makeItem(String name, int sellIn, int quality) {
+        if (name.equals("Aged Brie")) {
+            return new AgedBrie(name, sellIn, quality);
+        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return new Ticket(name, sellIn, quality);
+        } else if (name.equals("Sulfuras, Hand of Ragnaros")) {
+            return new LegendaryItem(name, sellIn);
+        } else {
+            return new NormalItem(name, sellIn, quality);
+        }
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+}
+
+class NormalItem extends Item {
+
+    public NormalItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void advanceDay() {
+        if (this.quality <= 0) {
+            this.sellIn--;
+            return;
+        }
+
+        if (this.sellIn < 0) {
+            this.quality -= 2;
+        } else {
+            this.quality--;
+        }
+
+        this.sellIn--;
+    }
+}
+
+class LegendaryItem extends Item {
+
+    public LegendaryItem(String name, int sellIn) {
+        super(name, sellIn, 80);
+    }
+
+    @Override
+    public void advanceDay() {}
+}
+
+class AgedBrie extends Item {
+
+    public AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void advanceDay() {
+        if (this.quality >= 50) {
+            this.sellIn--;
+            return;
+        }
+
+        if (this.sellIn < 0) {
+            this.quality += 2;
+        } else {
+            this.quality++;
+        }
+
+        this.sellIn--;
+    }
+}
+
+class Ticket extends Item {
+
+    public Ticket(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void advanceDay() {
+        if (this.sellIn < 0) {
+            this.quality = 0;
+        } else if (this.sellIn < 6) {
+            this.quality += 3;
+        } else if (this.sellIn < 11) {
+            this.quality += 2;
+        } else {
+            this.quality++;
+        }
+
+        this.sellIn--;
     }
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,27 +1,20 @@
 package com.gildedrose;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class GildedRoseTest {
 
-	@Test
-	public void test() {
+    @Test
+    public void test() {
+        Item[] items = new Item[] { Item.makeItem("naname", 0, 0) };
+        GildedRose gildedRose = new GildedRose(items);
 
-		Item[] items = new Item[]{
-				new Item("naname", 0, 0)
-		};
-		GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
 
-		gildedRose.updateQuality();
-
-		assertEquals(1, items.length);
-		assertEquals(-1, items[0].sellIn);
-		//assertEquals(0, items[0].quality);
-
-
-	}
-
+        assertEquals(1, items.length);
+        assertEquals(-1, items[0].sellIn);
+        //assertEquals(0, items[0].quality);
+    }
 }
