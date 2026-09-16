47420919ec68268e85256b7f9eea0975195a39ad
kimdoyoung0319 <kimdoyoung0319@gmail.com>
2026-09-16 14:56:10 +0900

    Added more test cases

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,11 +15,68 @@ public class GildedRoseTest {
 
         assertEquals(1, items.length);
         assertEquals(-1, items[0].sellIn);
-        //assertEquals(0, items[0].quality);
+        assertEquals(0, items[0].quality);
     }
 
     @Test
     public void updateQuality_AgedBrie_negativeSellIn_increasesQuality() {
-        Item[] items = new Item[] { Item.makeItem("Aged Brie", 2, 0) };
+        Item[] items = new Item[] { Item.makeItem("Aged Brie", 0, 0) };
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+
+        assertEquals(1, items.length);
+        assertEquals(-2, items[0].sellIn);
+        assertEquals(3, items[0].quality);
+    }
+
+    @Test
+    public void updateQuality_Legendary_qualityAndSellInUnchanged() {
+        Item[] items = new Item[] {
+            Item.makeItem("Sulfuras, Hand of Ragnaros", 0, 0),
+        };
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(1, items.length);
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[0].quality);
+    }
+
+    @Test
+    public void updateQuality_Ticket() {
+        Item[] items = new Item[] {
+            Item.makeItem("Backstage passes to a TAFKAL80ETC concert", 15, 0),
+        };
+        GildedRose gildedRose = new GildedRose(items);
+
+        for (int i = 0; i < 5; i++) {
+            gildedRose.updateQuality();
+        }
+
+        assertEquals(10, items[0].sellIn);
+        assertEquals(5, items[0].quality);
+
+        for (int i = 0; i < 5; i++) {
+            gildedRose.updateQuality();
+        }
+
+        assertEquals(5, items[0].sellIn);
+        assertEquals(15, items[0].quality);
+
+        for (int i = 0; i < 5; i++) {
+            gildedRose.updateQuality();
+        }
+
+        assertEquals(0, items[0].sellIn);
+        assertEquals(30, items[0].quality);
+
+        gildedRose.updateQuality(); // -1, 33
+        gildedRose.updateQuality(); // -2, 0
+
+        assertEquals(-2, items[0].sellIn);
+        assertEquals(0, items[0].quality);
     }
 }
